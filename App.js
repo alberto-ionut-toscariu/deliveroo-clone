b18c2943c719231f7faab77fc3bc96a4894b7ae6
Toscariu Alberto Ionut <alberto.toscariu@icis.com>
2023-07-29 16:09:03 +0300

    saving work

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
 import BasketScreen from './screens/BasketScreen';
 import PreparingOrderScreen from './screens/PreparingOrderScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
 import 'react-native-url-polyfill/auto';
 import { Provider } from 'react-redux';
 import { store } from './store';
@@ -20,8 +21,10 @@ export default function App() {
           <Stack.Screen name="Restaurant" component={RestaurantScreen} />
           <Stack.Screen name="Basket" component={BasketScreen}
             options={{ presentation: 'modal', headerShown: false }} />
-          <Stack.Screen name="PreparingOrder" component={PreparingOrderScreen} />
-
+          <Stack.Screen name="PreparingOrder" component={PreparingOrderScreen}
+            options={{ presentation: 'fullScreenModal', headerShown: false }} />
+          <Stack.Screen name="Delivery" component={DeliveryScreen}
+            options={{ presentation: 'fullScreenModal', headerShown: false }} />
 
         </Stack.Navigator>
       </Provider>
--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -91,7 +91,7 @@ const BasketScreen = () => {
                         <Text classname='font-extrabold'>{basketTotal + 5} lei</Text>
                     </View>
 
-                    <TouchableOpacity className='rounded-lg bg-[#00CCBB] p-4'>
+                    <TouchableOpacity onPress={navigation.navigate('PreparingOrder')} className='rounded-lg bg-[#00CCBB] p-4'>
                         <Text className='text-center text-white text-xl font-bold'>Place Order</Text>
                     </TouchableOpacity>
 
