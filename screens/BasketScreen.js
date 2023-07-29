545dcf2296180e3e74c8100cfe12951c8be5d54c
Toscariu Alberto Ionut <alberto.toscariu@icis.com>
2023-07-29 03:14:13 +0300

    building basket now

---
+++ screens/BasketScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+const BasketScreen = () => {
+    return (
+        <View>
+            <Text>BasketScreen</Text>
+        </View>
+    )
+}
+export default BasketScreen
