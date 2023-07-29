561c9c3b0115ebbc3c649800ef615991b66ca6ac
Toscariu Alberto Ionut <alberto.toscariu@icis.com>
2023-07-29 16:23:12 +0300

    added map and demo done

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { selectRestaurant } from '../features/restaurantSlice';
 import { XMarkIcon } from 'react-native-heroicons/outline';
 import * as Progress from 'react-native-progress';
+import MapView, { Marker } from 'react-native-maps';
 
 const DeliveryScreen = () => {
     const navigation = useNavigation();
@@ -38,7 +39,46 @@ const DeliveryScreen = () => {
                     </Text>
                 </View>
             </SafeAreaView>
+            <MapView
 
+                initialRegion={{
+                    latitude: restaurant.lat,
+                    longitude: restaurant.long,
+                    latitudeelta: 0.005,
+                    longitudeDelta: 0.005
+                }}
+                className='flex-1 mt-10 z-0'
+                mapType='mutedStandard'
+            >
+                <Marker
+                    coordinate={{
+                        latitude: restaurant.lat,
+                        longitude: restaurant.long,
+                    }}
+                    title={restaurant.title}
+                    description={restaurant.short_description}
+                    identifier='origin'
+                    pinColor='#00CCBB'
+                />
+            </MapView>
+            <SafeAreaView className='bg-white flex-row items-center space-x-5'>
+                <Image
+                    source={{
+                        uri: 'https://links.papareact.com/wru'
+                    }}
+                    className='h-12 w-12 bg-gray-300 rounded-full ml-5' />
+                <View className='flex-1'>
+                    <Text className='text-lg'>
+                        Toscariu Alberto
+                    </Text>
+                    <Text className='text-gray-400'>
+                        Your Rider
+                    </Text>
+                </View>
+                <Text className='text-[#00CCBB] text-lg mr-5 font-bold'>
+                    Call
+                </Text>
+            </SafeAreaView>
         </View>
     )
 }
